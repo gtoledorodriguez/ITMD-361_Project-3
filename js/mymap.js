8a2d6e7c8a12293191cc7631dc7eca8f17efcffa
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 17:48:56 -0600

    hopfully added an icon to my map

--- js/mymap.js
+++ js/mymap.js
@@ -3,6 +3,13 @@ var myLocation = {lat: 41.7906, lng: 87.5831};
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLocation,
-    zoom: 15
+    zoom: 18
   });
 }
+
+var marker = new google.maps.Marker({
+  position: myLocation,
+  map: myMap,
+  animation: google.maps.Animation.BOUNCE,
+  icon: 'images/msi.jpg'
+});
