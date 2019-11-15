211441b3c0e091befd150899e69b791f74624550
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 20:16:40 -0600

    changed functions to test if my code is working

--- js/mymap.js
+++ js/mymap.js
@@ -1,15 +1,23 @@
 var map;
-var myLocation = {lat: 41.7906, lng: 87.5831};
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLocation,
-    zoom: 18
+  var myLatLng = { lat: 41.8615, lng: -87.6136 };
+
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: myLatLng,
+    styles: mapstyle
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "The Balbo Monument"
+  });
+
+  var image = "images/msi.jpg";
+  var beachMarker = new google.maps.Marker({
+    position: { lat: 41.8615, lng: -87.6136 },
+    map: map,
+    icon: image
   });
 }
-
-var marker = new google.maps.Marker({
-  position: myLocation,
-  map: myMap,
-  animation: google.maps.Animation.BOUNCE,
-  icon: 'images/msi.jpg'
-});
