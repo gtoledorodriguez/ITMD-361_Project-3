58612ff97c423b07aa3669ee59ff0358f52c801c
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 23:20:08 -0600

    custom marker not working so commented out

--- js/mymap.js
+++ js/mymap.js
@@ -127,7 +127,8 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: myLocation,
     map: map,
-    title: "Museum of Science and Industry"
+    title: "Museum of Science and Industry",
+    //icon: image
   });
 
   var info = "The Museum of Science and Industry was originally constructed for the 1893 World's Columbian Exposition's 'White City', originally known as the  Palace of Fine Arts. The museum is the only building still standing from what once was the 'White City'. It was bought and opened by Julius Rosenwald, chairman of Sears, in 1933. He was inspired by a musuem he had seen in Europe and decided to create his own interactive museum. At the time of its opening, it only had one exhibit, the Coal Mine Exhibit which still exists today. Rosenwald had actual coal miners come in and be the tour guides for the exhibit. Now the museum has over 2,000 different exhibits!"
