3783b5a66ee60d0a4563052cee8fadd4e0438b9e
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 20:18:07 -0600

    removed unnecessary code for testing

--- js/mymap.js
+++ js/mymap.js
@@ -4,8 +4,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
-    center: myLatLng,
-    styles: mapstyle
+    center: myLatLng
   });
 
   var marker = new google.maps.Marker({
