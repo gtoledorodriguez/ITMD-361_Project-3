59d4e738214e1ef15919913dba354d438899ae7b
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 21:39:17 -0600

    changed latlong, deleted styel

--- js/mymap.js
+++ js/mymap.js
@@ -1,11 +1,10 @@
 var map;
 function initMap() {
-  var myLatLng = { lat: 41.8615, lng: -87.6136 };
+  var myLatLng = { lat: 41.7906, lng: 87.5831 };
 
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
-    center: myLatLng,
-    styles: mapstyle
+    center: myLatLng
   });
 
   var marker = new google.maps.Marker({
@@ -20,6 +19,9 @@ function initMap() {
     map: map,
     icon: image
   });
+
+  var info = "The Museum of Science and Industry was originally constructed for the 1893 World's Columbian Exposition's 'White City', originally known as the  Palace of Fine Arts. The museum is the only building still standing from what once was the 'White City'. It was bought and opened by Julius Rosenwald, chairman of Sears, in 1933. He was inspired by a musuem he had saw in Europe and decided to create his own interactive museum. At the time of its opening, it only had one exhibit, the Coal Mine Exhibit which still exists today. Rosenwald had actual coal miner come in and be the tour guides for the exhibit. Now the museum has over 2,000 different exhibits!"
+
 }
 
 const mapstyle = [
