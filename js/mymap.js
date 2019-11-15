dcb00be342f5d2a867061fd5f41d0864e8dbf9c3
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-14 23:15:32 -0600

    fixed spelling

--- js/mymap.js
+++ js/mymap.js
@@ -130,7 +130,7 @@ function initMap() {
     title: "Museum of Science and Industry"
   });
 
-  var info = "The Museum of Science and Industry was originally constructed for the 1893 World's Columbian Exposition's 'White City', originally known as the  Palace of Fine Arts. The museum is the only building still standing from what once was the 'White City'. It was bought and opened by Julius Rosenwald, chairman of Sears, in 1933. He was inspired by a musuem he had saw in Europe and decided to create his own interactive museum. At the time of its opening, it only had one exhibit, the Coal Mine Exhibit which still exists today. Rosenwald had actual coal miner come in and be the tour guides for the exhibit. Now the museum has over 2,000 different exhibits!"
+  var info = "The Museum of Science and Industry was originally constructed for the 1893 World's Columbian Exposition's 'White City', originally known as the  Palace of Fine Arts. The museum is the only building still standing from what once was the 'White City'. It was bought and opened by Julius Rosenwald, chairman of Sears, in 1933. He was inspired by a musuem he had seen in Europe and decided to create his own interactive museum. At the time of its opening, it only had one exhibit, the Coal Mine Exhibit which still exists today. Rosenwald had actual coal miners come in and be the tour guides for the exhibit. Now the museum has over 2,000 different exhibits!"
 
   var infowindow = new google.maps.InfoWindow({
     content: info
